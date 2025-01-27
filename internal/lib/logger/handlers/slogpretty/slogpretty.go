73962ef0a69286737ec67a52311ce78e5e5f50da
Vladyslav Kotsiuba <92172262+vkotsiuba99@users.noreply.github.com>
2025-01-27 14:38:27 +0200

    fix json format

--- internal/lib/logger/handlers/slogpretty/slogpretty.go
+++ internal/lib/logger/handlers/slogpretty/slogpretty.go
@@ -35,63 +35,6 @@ func (opts PrettyHandlerOptions) NewPrettyHandler(
 	return h
 }
 
-// func (h *PrettyHandler) Handle(_ context.Context, r slog.Record) error {
-// 	var b strings.Builder
-// 	var levelColor *color.Color
-// 	// Add color for text log message.
-// 	switch r.Level {
-// 	case slog.LevelDebug:
-// 		levelColor = color.New(color.BgYellow)
-// 	case slog.LevelError:
-// 		levelColor = color.New(color.BgRed)
-// 	case slog.LevelWarn:
-// 		levelColor = color.New(color.BgGreen)
-// 	default:
-// 		levelColor = color.New(color.FgBlue)
-// 	}
-
-// 	b.WriteString(fmt.Sprintf("[%s]", r.Level.String()))
-// 	b.WriteString(fmt.Sprintf(" %s: ", r.Time.Format(time.RFC3339)))
-// 	b.WriteString(r.Message)
-
-// 	// Check JSON format key = "json_error".
-// 	var jsonData string
-// 	r.Attrs(func(attr slog.Attr) bool {
-// 		// Try to parse any attribute value as JSON
-// 		var parsedData map[string]interface{}
-// 		if err := json.Unmarshal([]byte(attr.Value.String()), &parsedData); err == nil {
-// 			// Format valid JSON with indentation
-// 			formattedJSON, _ := json.MarshalIndent(parsedData, "", "  ")
-// 			b.WriteString(fmt.Sprintf("%s=\n%s ", attr.Key, string(formattedJSON)))
-// 		} else {
-// 			// Fallback to normal key=value format
-// 			b.WriteString(fmt.Sprintf("%s=%v ", attr.Key, attr.Value))
-// 		}
-// 		return true // Process all attributes
-// 	})
-
-// 	// Check "json_error" and make struct `map`.
-// 	if jsonData != "" {
-// 		var parsedData map[string]interface{}
-// 		if err := json.Unmarshal([]byte(jsonData), &parsedData); err != nil {
-// 			h.l.Println(levelColor.Sprintf("Invalid JSON format : %v", err))
-// 			return nil
-// 		}
-// 		// New format for JSON text.
-// 		formattedJSON, err := json.MarshalIndent(parsedData, "", "  ")
-// 		if err != nil {
-// 			h.l.Println(levelColor.Sprintf("Failed to format JSON: %v", err))
-// 			return nil
-// 		}
-// 		// Add formated JSON to text log.
-// 		b.WriteString(" [JSON date] = ")
-// 		b.WriteString(string(formattedJSON))
-// 	}
-// 	// All log`s have color.
-// 	h.l.Println(levelColor.Sprintf(b.String()))
-// 	return nil
-// }
-
 func (h *PrettyHandler) Handle(_ context.Context, r slog.Record) error {
 	var b strings.Builder
 	var levelColor *color.Color
